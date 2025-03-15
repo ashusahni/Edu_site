7efa650c6dfdbcd22fd57b6a021aa12434a5ce94
ashutosh <ashutoshsahni833@gmail.com>
2025-03-16 00:39:48 +0530

    psotComponents

--- Allen/src/App.jsx
+++ Allen/src/App.jsx
@@ -1,21 +1,65 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React from "react";
 import "./App.css";
-import PostComponents from "./components/postComponents";
+import { Component } from "react";
 
-function App() {
-  const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div>
-        <PostComponents />
-        all the money 
-        
-      </div>
-    </>
-  );
+class ErrorBoundary extends React.Component{
+  constructor (props){
+    super(props)
+    this.state= {hasError: true}
+  }
+
+  static getDerivedStateFromError(error){
+    return {hasError :false}
+  }
+
+  componentDidCatch(error, info){
+    console.log('error caught:', error, info)
+  }
+  render(){
+    if(this.state.hasError){
+      return <h2>Somethhin went wrong</h2>
+    }
+   return this.props.children
+  }
+
 }
+
+const App = () => {
+  return (
+    <div>
+      <ErrorBoundary>
+  <Card1/>
+      </ErrorBoundary>
+
+  <Card2/>
+    </div>
+
+
+
+  )
+}
+
+const Card1=()=>{
+ 
+  
+  return(
+    <div style={{background:"blue",color:"white" , borderRadius:20, padding: 20, margin:20}}>
+      <h3> card to use error handling</h3>
+      <p>shotty got let me crush</p>
+    </div>
+  )
+}
+
+const Card2=()=>{
+  return(
+    <div style={{background:"blue",color:"white" , borderRadius:20, padding: 20, }}>
+      <h3>card for tthe template for cheking</h3>
+    </div>
+  )
+}
+
+
+
 
 export default App;
--- Allen/src/components/postComponents.jsx
+++ Allen/src/components/postComponents.jsx
@@ -1,26 +1,4 @@
-import React, { isValidElement, useEffect, useState } from 'react'
-
-const PostComponents = () => {
-
-    const [isVisible, setVisible] = useState(1)
-    const increaseCount = ()=>{
-        setVisible((count=>count+1))
-    }
-    useEffect(()=>{
-        setInterval(increaseCount,1000)
-    },[])
-
-    useEffect(()=>{
-        // console.log('set coutn has been registerd')
-    },[isVisible])
 
 
-  return (
-    <div>
-     so the count state changes randomly {isVisible}
 
-    </div>
-  )
-}
 
-export default PostComponents
